52bb222d33b689c630a6fe3f180fab58a816b4f6
Marcelo-Alves-Junior <marcelo.cont@live.com>
2023-09-30 00:43:28 +0000

    Atualização Aula 29

--- 09.2023/Dia 28/main.py
+++ 09.2023/Dia 28/main.py
@@ -17,7 +17,7 @@ def perimetro (a,b,c):
 '''3) Crie uma função que retorna qual o conceito dada uma nota. Utilize a tabela a seguir: A >=9, B>=8, C>=7, D.=6, F<6 .'''
 
 def conceito (nota):
-    if nota >= 9:
+    if nota >= 9 and nota<=10:
         conceito = 'A'
     elif nota >=8:
         conceito = 'B'
@@ -27,6 +27,7 @@ def conceito (nota):
         conceito = 'D'
     else:
         conceito = 'F'
+    return conceito
 
 '''4) Crie uma função que retorna 1 se o aluno for aprovado em uma disciplina e 0 caso contrário, considerando que as seguintes informações são passadas como argumentos:
 * o número total de aulas de uma disciplina;
@@ -50,5 +51,30 @@ def classe_eleitoral (idade):
         classe = 'Eleitor Facultativo'
     else:
         classe = 'Eleitor obrigatório'
+      
     return classe
 
+'''6) Escreva uma função que receba um número e retorne -1 se o número for ímpar ou 1
+se for par.'''
+
+def impar_par (numero):
+  resto = numero % 2
+  if resto > 0:
+    aux = -1
+  else:
+    aux = 1
+  return aux
+
+a = int(input('Informe um número: '))
+
+a = int(input('Informe um número')
+print(impar_par(a))
+
+'''7) Escreva uma função para calcular o fatorial de um número. Lembre que fatorial de
+número negativo não existe.'''
+
+
+
+'''8) Séries de Taylor são vistas na disciplina de cálculo e são muito aplicáveis no campo da
+informática. Uma série de Taylor pode ser utilizada para calcular o valor de seno de um
+número em radianos. A série de Taylor para função seno é:'''
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("10 - Escrever um algoritmo que lê um valor N inteiro e positivo e que calcula e escreve o valor de E. E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / N!")
+'''print("10 - Escrever um algoritmo que lê um valor N inteiro e positivo e que calcula e escreve o valor de E. E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / N!")
 
 n = int (input("Informe um número:"))
 
@@ -10,4 +10,16 @@ for _ in range(n):
   while i < n:
     fat = fat * (n - i)
     i = i + 1
-print (fat)
+print (fat)'''
+
+def impar_par (numero):
+  resto = numero % 2
+  if resto > 0:
+    aux = -1
+  else:
+    aux = 1
+  return aux
+
+a = int(input('Informe um número: '))
+        
+print("o numero é", impar_par(a))
